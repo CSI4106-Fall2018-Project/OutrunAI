d3add44b18d6864bbe11b3be596c246e8705708e
Cooper Lawrence <clawr014@uottawa.ca>
2018-11-08 09:50:46 -0500

    Added main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from Display import Display
+
+#Program entry point
+def main():
+	display = Display("Cannonball", 15)
+	display.run()
+
+#Called upon runtime
+if (__name__ == "__main__"):
+	main()
