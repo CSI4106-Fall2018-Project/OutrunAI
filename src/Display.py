3a65b750f7f83105dc240462113cebc7ef79b717
Cooper Lawrence <clawr014@uottawa.ca>
2018-11-12 22:29:21 -0500

    Updated line filtering to remove noise, added estimated road curvature

--- src/Display.py
+++ src/Display.py
@@ -72,28 +72,49 @@ class Display:
 	#Takes the raw image capture and isolates the road lines TODO: Create ROI below horizon
 	def filterLines(self, inputFrame):
 		if (inputFrame.all() != None):
-			outputFrame = cv.cvtColor(inputFrame, cv.COLOR_RGB2GRAY) #Convert to grayscale
-
-			ret, outputFrame = cv.threshold(outputFrame, 245, 255, cv.THRESH_BINARY) #Threshold the image to find the lines
-			outputFrame = cv.GaussianBlur(outputFrame, (5, 5), 0) #Remove sharp edges
-
-			#Remove noise
-			kernel = np.ones((3, 3), np.uint8)
-			outputFrame = cv.morphologyEx(outputFrame, cv.MORPH_OPEN, kernel)
-
 			height, width = inputFrame.shape[:2]
 
-			for y in range(height//3 * 2, height, 5):
+			outputFrame = cv.cvtColor(inputFrame, cv.COLOR_RGB2GRAY) #Convert to grayscale
+
+			outputFrame = cv.GaussianBlur(outputFrame, (3, 3), 0) #Remove sharp edges
+
+			ret, outputFrame = cv.threshold(outputFrame, 245, 255, cv.THRESH_BINARY) #Threshold the image to find the lines
+
+			outputFrame, contours, hierarchy = cv.findContours(outputFrame, 1, 2)
+
+			outputFrame = np.zeros((height, width, 1), np.uint8)
+
+			for cnt in contours:
+				#moments = cv.moments(cnt)
+
+				area = cv.contourArea(cnt)
+				if (area > 0 and area < 300):
+					rect = cv.minAreaRect(cnt)
+					box = cv.boxPoints(rect)
+					box = np.int0(cnt)
+					cv.drawContours(outputFrame, [box], 0, (255, 255, 255), -1)
+
+			#Average out the road lines TODO: Use rate of change to smooth values
+			roadCurvature = 0
+			for k in range(height//3 * 2, height - height//8, 10):
 				whiteSum = 0
-				whitePixels = 1
-				for x in range(0, width):
-					if (outputFrame.item(y, x) > 0):
-						whiteSum += x
-						whitePixels += 1
-				whiteAverage = whiteSum // whitePixels
+				whitePixels = 0
 
-				cv.circle(outputFrame, (whiteAverage, y), 2, (255, 255, 255), -1, 8, 0)
+				for y in range(k, k + 10):
+					for x in range(0, width):
+						if (outputFrame.item(y, x, 0) > 0):
+							whiteSum += x
+							whitePixels += 1
 
+				if (whitePixels > 0):
+					whiteAverage = whiteSum// whitePixels
+					tmpCurve = (width//2 - whiteAverage) * ( k - (height//3 * 2))
+
+					if (abs(tmpCurve) > abs(roadCurvature)//20 or abs(tmpCurve) < abs(roadCurvature)//20):
+						roadCurvature += tmpCurve
+						cv.circle(outputFrame, (whiteAverage, y), 2, (255, 255, 255), -1, 8, 0)
+
+			print("Road curvature: ", roadCurvature)
 			return outputFrame
 		else:
 			print("No frame supplied!")
