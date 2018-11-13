a9dc3912903fc2ef1531d5efffacd4b2ee378751
Cooper Lawrence <clawr014@uottawa.ca>
2018-11-12 22:54:52 -0500

    Added template for memory scanner

---
+++ src/MemoryScanner.py
@@ -0,0 +1,29 @@
+
+class MemoryScanner:
+
+	#Constructor
+	def __init__(self):
+		self.timer = 0
+		self.score = 0
+		self.lapTime = 0
+		self.speed = 0
+		self.stage = 0
+
+	#Updates all game values
+	def updateValues(self):
+		pass
+
+	def readTimer(self):
+		pass
+
+	def readScore(self):
+		pass
+
+	def readLapTime(self):
+		pass
+
+	def readSpeed(self):
+		pass
+
+	def readStage(self):
+		pass
