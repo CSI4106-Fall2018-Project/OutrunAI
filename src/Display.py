9ac3f0c68cc002863406bb05a2aec9d53454a7d9
Justin Huynh <polar.truth@gmail.com>
2018-11-15 15:34:22 -0500

    Updated Display to have curvature and car position

--- src/Display.py
+++ src/Display.py
@@ -21,11 +21,24 @@ class Display:
 
 			rawFrame = self.capturer.getFrame()
 
-			#horizon = self.findHorizon(rawFrame)
-
 			roadFrame = self.filterLines(rawFrame)
 
-			cv.imshow("Road Frame", roadFrame)
+			roadCurvature, carPosition, outputFrame = self.calculateCurvature(roadFrame)
+
+			print("Road curvature: ", roadCurvature, " Car position: ", carPosition)
+
+			#Extremely simple decision tree for controlling the car's steering
+			# if (vxAvg[0] < -0.05):
+			# 	print("Left")
+			# 	Controller.left()
+			# elif (vxAvg[0] > 0.05):
+			# 	print("Right")
+			# 	Controller.right()
+			# else:
+			# 	print("Straight")
+			# 	Controller.straight()
+
+			cv.imshow("Road Frame", outputFrame)
 
 			#Should ALWAYS be called last (to ensure accurate synchronization and avoid undetectable delays)
 			self.syncClock(startTime, time.time(), False)
@@ -45,149 +58,62 @@ class Display:
 
 		if (verbose == True):
 			print("FPS: ", displayFps, " Slept: ", sleepTime, "ms")
-	
+
 		return displayFps
 
-	#Takes the raw image capture and finds the position of the horizion
-	def findHorizon(self, inputFrame): #TODO: Update horizon detection based on new limited scope
-		if (inputFrame.all() != None):
-			#Do stuff
-			horizon = cv.cvtColor(inputFrame, cv.COLOR_RGB2GRAY)
-			horizon = cv.GaussianBlur(horizon, (5, 5), 0) #Remove sharp edges
-
-			height, width = inputFrame.shape[:2]
-
-			for i in range(0, height - height / 12, height - 12):
-				lines = cv.HoughLinesP(horizon[height//2:height, 0:width], 1, 3.14 / 180, 20, 100, 400)
-
-				for line in lines:
-					position = line[0]
-					cv.line(horizon, (position[0], position[1]), (position[2], position[3]), (0, 0, 0), 1)
-
-			cv.imshow("Horizon image", horizon)
-			return -1
-		else:
-			print("No frame supplied!")
-			return None
-
-	#Takes the raw image capture and isolates the road lines TODO: Create ROI below horizon
+	#Takes the raw image capture and isolates the road lines
 	def filterLines(self, inputFrame):
 		if (inputFrame.all() != None):
-			height, width = inputFrame.shape[:2]
-
 			outputFrame = cv.cvtColor(inputFrame, cv.COLOR_RGB2GRAY) #Convert to grayscale
 
 			ret, outputFrame = cv.threshold(outputFrame, 245, 255, cv.THRESH_BINARY) #Threshold the image to find the lines
-
-			outputFrame, contours, hierarchy = cv.findContours(outputFrame, 0, 2)
-
-			#outputFrame = np.zeros((height, width, 1), np.uint8)
-
-			vxAvg = 0
-			vyAvg = 0
-			xAvg = 0
-			yAvg = 0
-			numberPoints = 0
-			for cnt in contours:
-				#check blob size
-				if (cv.contourArea(cnt) > 30):
-
-					moment = cv.moments(cnt) #Calculate the moment of each blob
-
-					[vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2, 0, 0.01, 0.01) #Fit a straight line to each moment
-
-					if (x > int(width/2)):
-						vx *= -1;
-						vy *= -1;
-
-					if (y > height//3 and abs(vx) > 0.05 and abs(vx) < 0.99):
-						cv.arrowedLine(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 2, tipLength=0.5) #Display the direction of each moment
-						#cv.line(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 3)
-						cv.putText(outputFrame, str(vx[0])[:5], (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-						
-						vxAvg += vx
-						vyAvg += vy
-						xAvg += x
-						yAvg += y
-						numberPoints += 1
-
-			if (numberPoints > 0):
-				vxAvg /= -numberPoints
-				vyAvg /= numberPoints
-				xAvg /= numberPoints
-				yAvg /= numberPoints
-
-				cv.line(outputFrame, (xAvg, yAvg), (xAvg + (20 * vxAvg), yAvg + (20 * vyAvg)), (255, 255, 255), 1)
-
-				#print("Road curvature: ", width//2 - xAvg / numberPoints)
-				print("Road curvature: ", vxAvg)
-
-			#TODO: Work on the following experimental curve detection
-			# rvxAvg = 0
-			# rvyAvg = 0
-			# rxAvg = 0
-			# ryAvg = 0
-			# rNumberPoints = 0
-
-			# lvxAvg = 0
-			# lvyAvg = 0
-			# lxAvg = 0
-			# lyAvg = 0
-			# lNumberPoints = 0
-			# for cnt in contours:
-			# 	#check blob size
-			# 	if (cv.contourArea(cnt) > 20):
-
-			# 		moment = cv.moments(cnt) #Calculate the moment of each blob
-
-			# 		[vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2, 0, 0.01, 0.01) #Fit a straight line to each moment
-
-			# 		if (y > height//3 and abs(vx) > 0.05 and abs(vx) < 0.99):
-			# 			if (x > width//2):
-			# 				vx *= -1;
-			# 				vy *= -1;
-
-			# 				rvxAvg += vx
-			# 				rvyAvg += vy
-			# 				rxAvg += x
-			# 				ryAvg += y
-			# 				rNumberPoints += 1
-			# 			else:
-			# 				lvxAvg += vx
-			# 				lvyAvg += vy
-			# 				lxAvg += x
-			# 				lyAvg += y
-			# 				lNumberPoints += 1
-
-
-			# 			cv.arrowedLine(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 2, tipLength=0.5) #Display the direction of each moment
-			# 			#cv.line(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 3)
-			# 			cv.putText(outputFrame, str(vx[0])[:5], (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-			
-			# if (rNumberPoints > 0):
-			# 	rvxAvg /= -rNumberPoints
-			# 	rvyAvg /= rNumberPoints
-			# 	rxAvg /= rNumberPoints
-			# 	ryAvg /= rNumberPoints
-
-			# if (lNumberPoints > 0):
-			# 	lvxAvg /= -lNumberPoints
-			# 	lvyAvg /= lNumberPoints
-			# 	lxAvg /= lNumberPoints
-			# 	lyAvg /= lNumberPoints
-
-			# if (rNumberPoints + lNumberPoints > 0):
-			# 	vxAvg = (rvxAvg + lvxAvg)/2
-			# 	vyAvg = (rvyAvg + lvyAvg)/2
-			# 	xAvg = (rxAvg + lxAvg)/2
-			# 	yAvg = (ryAvg + lyAvg)/2
-
-			# 	cv.line(outputFrame, (xAvg, yAvg), (xAvg + (20 * vxAvg), yAvg + (20 * vyAvg)), (255, 255, 255), 1)
-
-			# 	#print("Road curvature: ", width//2 - xAvg / numberPoints)
-			# 	print("Road curvature: ", vxAvg)
 
 			return outputFrame
 		else:
 			print("No frame supplied!")
 			return None
+
+	#Calculates
+	def calculateCurvature(self, inputFrame):
+		height, width = inputFrame.shape[:2]
+
+		outputFrame, contours, hierarchy = cv.findContours(inputFrame, 0, 2)
+
+		vxAvg = 0
+		vyAvg = 0
+		xAvg = 0
+		yAvg = 0
+		numberPoints = 0
+
+		for cnt in contours:
+			#Check that the contour actually represents a line
+			if (cv.contourArea(cnt) > 30):
+
+				moment = cv.moments(cnt) #Calculate the moment of each blob
+
+				[vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2, 0, 0.01, 0.01) #Fit a straight line to each moment
+
+				if (x > int(width/2)):
+					vx *= -1;
+					vy *= -1;
+
+				if (y > height//3 and abs(vx) > 0.05 and abs(vx) < 0.99):
+					cv.arrowedLine(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 2, tipLength=0.5) #Display the direction of each moment
+					#cv.line(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 3)
+					cv.putText(outputFrame, str(vx[0])[:5], (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+
+					vxAvg += vx
+					vyAvg += vy
+					xAvg += x
+					yAvg += y
+					numberPoints += 1
+
+		if (numberPoints > 0):
+			vxAvg /= -numberPoints
+			vyAvg /= numberPoints
+			xAvg /= numberPoints
+			yAvg /= numberPoints
+
+			return vxAvg[0], xAvg, outputFrame
+
+		return -99, -99, outputFrame #Default return (no lines found)
