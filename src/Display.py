21e07085f7ab59139fa08d78320d8f231df6f3ae
Cooper Lawrence <clawr014@uottawa.ca>
2018-11-14 19:08:08 -0500

    Updated curve detection, 75% accurate

--- src/Display.py
+++ src/Display.py
@@ -5,6 +5,7 @@ import time #For timing analysis and framerate synchronization
 import cv2 as cv #OpenCV for image processing
 
 from Capturer import Capturer #Capturer to grab frames
+from Controller import Controller
 
 class Display:
 	#Constructor
@@ -48,7 +49,7 @@ class Display:
 		return displayFps
 
 	#Takes the raw image capture and finds the position of the horizion
-	def findHorizon(self, inputFrame):
+	def findHorizon(self, inputFrame): #TODO: Update horizon detection based on new limited scope
 		if (inputFrame.all() != None):
 			#Do stuff
 			horizon = cv.cvtColor(inputFrame, cv.COLOR_RGB2GRAY)
@@ -76,45 +77,116 @@ class Display:
 
 			outputFrame = cv.cvtColor(inputFrame, cv.COLOR_RGB2GRAY) #Convert to grayscale
 
-			outputFrame = cv.GaussianBlur(outputFrame, (3, 3), 0) #Remove sharp edges
-
 			ret, outputFrame = cv.threshold(outputFrame, 245, 255, cv.THRESH_BINARY) #Threshold the image to find the lines
 
-			outputFrame, contours, hierarchy = cv.findContours(outputFrame, 1, 2)
+			outputFrame, contours, hierarchy = cv.findContours(outputFrame, 0, 2)
 
-			outputFrame = np.zeros((height, width, 1), np.uint8)
+			#outputFrame = np.zeros((height, width, 1), np.uint8)
 
+			vxAvg = 0
+			vyAvg = 0
+			xAvg = 0
+			yAvg = 0
+			numberPoints = 0
 			for cnt in contours:
-				#moments = cv.moments(cnt)
+				#check blob size
+				if (cv.contourArea(cnt) > 30):
 
-				area = cv.contourArea(cnt)
-				if (area > 0 and area < 300):
-					rect = cv.minAreaRect(cnt)
-					box = cv.boxPoints(rect)
-					box = np.int0(cnt)
-					cv.drawContours(outputFrame, [box], 0, (255, 255, 255), -1)
+					moment = cv.moments(cnt) #Calculate the moment of each blob
 
-			#Average out the road lines TODO: Use rate of change to smooth values
-			roadCurvature = 0
-			for k in range(height//3 * 2, height - height//8, 10):
-				whiteSum = 0
-				whitePixels = 0
+					[vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2, 0, 0.01, 0.01) #Fit a straight line to each moment
 
-				for y in range(k, k + 10):
-					for x in range(0, width):
-						if (outputFrame.item(y, x, 0) > 0):
-							whiteSum += x
-							whitePixels += 1
+					if (x > int(width/2)):
+						vx *= -1;
+						vy *= -1;
 
-				if (whitePixels > 0):
-					whiteAverage = whiteSum// whitePixels
-					tmpCurve = (width//2 - whiteAverage) * ( k - (height//3 * 2))
+					if (y > height//3 and abs(vx) > 0.05 and abs(vx) < 0.99):
+						cv.arrowedLine(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 2, tipLength=0.5) #Display the direction of each moment
+						#cv.line(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 3)
+						cv.putText(outputFrame, str(vx[0])[:5], (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+						
+						vxAvg += vx
+						vyAvg += vy
+						xAvg += x
+						yAvg += y
+						numberPoints += 1
 
-					if (abs(tmpCurve) > abs(roadCurvature)//20 or abs(tmpCurve) < abs(roadCurvature)//20):
-						roadCurvature += tmpCurve
-						cv.circle(outputFrame, (whiteAverage, y), 2, (255, 255, 255), -1, 8, 0)
+			if (numberPoints > 0):
+				vxAvg /= -numberPoints
+				vyAvg /= numberPoints
+				xAvg /= numberPoints
+				yAvg /= numberPoints
 
-			print("Road curvature: ", roadCurvature)
+				cv.line(outputFrame, (xAvg, yAvg), (xAvg + (20 * vxAvg), yAvg + (20 * vyAvg)), (255, 255, 255), 1)
+
+				#print("Road curvature: ", width//2 - xAvg / numberPoints)
+				print("Road curvature: ", vxAvg)
+
+			#TODO: Work on the following experimental curve detection
+			# rvxAvg = 0
+			# rvyAvg = 0
+			# rxAvg = 0
+			# ryAvg = 0
+			# rNumberPoints = 0
+
+			# lvxAvg = 0
+			# lvyAvg = 0
+			# lxAvg = 0
+			# lyAvg = 0
+			# lNumberPoints = 0
+			# for cnt in contours:
+			# 	#check blob size
+			# 	if (cv.contourArea(cnt) > 20):
+
+			# 		moment = cv.moments(cnt) #Calculate the moment of each blob
+
+			# 		[vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2, 0, 0.01, 0.01) #Fit a straight line to each moment
+
+			# 		if (y > height//3 and abs(vx) > 0.05 and abs(vx) < 0.99):
+			# 			if (x > width//2):
+			# 				vx *= -1;
+			# 				vy *= -1;
+
+			# 				rvxAvg += vx
+			# 				rvyAvg += vy
+			# 				rxAvg += x
+			# 				ryAvg += y
+			# 				rNumberPoints += 1
+			# 			else:
+			# 				lvxAvg += vx
+			# 				lvyAvg += vy
+			# 				lxAvg += x
+			# 				lyAvg += y
+			# 				lNumberPoints += 1
+
+
+			# 			cv.arrowedLine(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 2, tipLength=0.5) #Display the direction of each moment
+			# 			#cv.line(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 3)
+			# 			cv.putText(outputFrame, str(vx[0])[:5], (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+			
+			# if (rNumberPoints > 0):
+			# 	rvxAvg /= -rNumberPoints
+			# 	rvyAvg /= rNumberPoints
+			# 	rxAvg /= rNumberPoints
+			# 	ryAvg /= rNumberPoints
+
+			# if (lNumberPoints > 0):
+			# 	lvxAvg /= -lNumberPoints
+			# 	lvyAvg /= lNumberPoints
+			# 	lxAvg /= lNumberPoints
+			# 	lyAvg /= lNumberPoints
+
+			# if (rNumberPoints + lNumberPoints > 0):
+			# 	vxAvg = (rvxAvg + lvxAvg)/2
+			# 	vyAvg = (rvyAvg + lvyAvg)/2
+			# 	xAvg = (rxAvg + lxAvg)/2
+			# 	yAvg = (ryAvg + lyAvg)/2
+
+			# 	cv.line(outputFrame, (xAvg, yAvg), (xAvg + (20 * vxAvg), yAvg + (20 * vyAvg)), (255, 255, 255), 1)
+
+			# 	#print("Road curvature: ", width//2 - xAvg / numberPoints)
+			# 	print("Road curvature: ", vxAvg)
+
 			return outputFrame
 		else:
 			print("No frame supplied!")
