8f48b13e9bff3e999fedd698529b0c1e4b311064
Cooper Lawrence <clawr014@uottawa.ca>
2018-11-15 13:20:19 -0500

    Split lane detection and curve/ car position detection into two methods

--- src/Display.py
+++ src/Display.py
@@ -23,7 +23,22 @@ class Display:
 
 			roadFrame = self.filterLines(rawFrame)
 
-			cv.imshow("Road Frame", roadFrame)
+			roadCurvature, carPosition, outputFrame = self.calculateCurvature(roadFrame)
+
+			print("Road curvature: ", roadCurvature, " Car position: ", carPosition)
+
+			#Extremely simple decision tree for controlling the car's steering
+			# if (vxAvg[0] < -0.05):
+			# 	print("Left")
+			# 	Controller.left()
+			# elif (vxAvg[0] > 0.05):
+			# 	print("Right")
+			# 	Controller.right()
+			# else:
+			# 	print("Straight")
+			# 	Controller.straight()
+
+			cv.imshow("Road Frame", outputFrame)
 
 			#Should ALWAYS be called last (to ensure accurate synchronization and avoid undetectable delays)
 			self.syncClock(startTime, time.time(), False)
@@ -46,67 +61,59 @@ class Display:
 	
 		return displayFps
 
-	#Takes the raw image capture and isolates the road lines TODO: Create ROI below horizon
+	#Takes the raw image capture and isolates the road lines
 	def filterLines(self, inputFrame):
 		if (inputFrame.all() != None):
-			height, width = inputFrame.shape[:2]
-
 			outputFrame = cv.cvtColor(inputFrame, cv.COLOR_RGB2GRAY) #Convert to grayscale
 
 			ret, outputFrame = cv.threshold(outputFrame, 245, 255, cv.THRESH_BINARY) #Threshold the image to find the lines
-
-			outputFrame, contours, hierarchy = cv.findContours(outputFrame, 0, 2)
-
-			vxAvg = 0
-			vyAvg = 0
-			xAvg = 0
-			yAvg = 0
-			numberPoints = 0
-			for cnt in contours:
-				#check blob size
-				if (cv.contourArea(cnt) > 30):
-
-					moment = cv.moments(cnt) #Calculate the moment of each blob
-
-					[vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2, 0, 0.01, 0.01) #Fit a straight line to each moment
-
-					if (x > int(width/2)):
-						vx *= -1;
-						vy *= -1;
-
-					if (y > height//3 and abs(vx) > 0.05 and abs(vx) < 0.99):
-						cv.arrowedLine(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 2, tipLength=0.5) #Display the direction of each moment
-						#cv.line(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 3)
-						cv.putText(outputFrame, str(vx[0])[:5], (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-						
-						vxAvg += vx
-						vyAvg += vy
-						xAvg += x
-						yAvg += y
-						numberPoints += 1
-
-			if (numberPoints > 0):
-				vxAvg /= -numberPoints
-				vyAvg /= numberPoints
-				xAvg /= numberPoints
-				yAvg /= numberPoints
-
-				cv.line(outputFrame, (xAvg, yAvg), (xAvg + (50 * vxAvg), yAvg + (50 * vyAvg)), (255, 255, 255), 2)
-
-				print("Road curvature: ", vxAvg[0], " Car position: ", width//2 - xAvg[0])
-
-				#Extremely simple decision tree for controlling the car's steering
-				# if (vxAvg[0] < -0.05):
-				# 	print("Left")
-				# 	Controller.left()
-				# elif (vxAvg[0] > 0.05):
-				# 	print("Right")
-				# 	Controller.right()
-				# else:
-				# 	print("Straight")
-				# 	Controller.straight()
 
 			return outputFrame
 		else:
 			print("No frame supplied!")
 			return None
+
+	#Calculates 
+	def calculateCurvature(self, inputFrame):
+		height, width = inputFrame.shape[:2]
+
+		outputFrame, contours, hierarchy = cv.findContours(inputFrame, 0, 2)
+
+		vxAvg = 0
+		vyAvg = 0
+		xAvg = 0
+		yAvg = 0
+		numberPoints = 0
+
+		for cnt in contours:
+			#Check that the contour actually represents a line
+			if (cv.contourArea(cnt) > 30):
+
+				moment = cv.moments(cnt) #Calculate the moment of each blob
+
+				[vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2, 0, 0.01, 0.01) #Fit a straight line to each moment
+
+				if (x > int(width/2)):
+					vx *= -1;
+					vy *= -1;
+
+				if (y > height//3 and abs(vx) > 0.05 and abs(vx) < 0.99):
+					cv.arrowedLine(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 2, tipLength=0.5) #Display the direction of each moment
+					#cv.line(outputFrame, (x, y), (x + (20 * vx), y + (20 * vy)), (255, 255, 255), 3)
+					cv.putText(outputFrame, str(vx[0])[:5], (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+						
+					vxAvg += vx
+					vyAvg += vy
+					xAvg += x
+					yAvg += y
+					numberPoints += 1
+
+		if (numberPoints > 0):
+			vxAvg /= -numberPoints
+			vyAvg /= numberPoints
+			xAvg /= numberPoints
+			yAvg /= numberPoints
+
+			return vxAvg[0], xAvg, outputFrame
+		
+		return -99, -99, outputFrame #Default return (no lines found)
