877d4c2bf1d6028f01616e1e2b9835d193314642
Cooper Lawrence <clawr014@uottawa.ca>
2018-11-19 20:26:17 -0500

    Added comments, added ability to fit, predict, dumpModel, and loadModel

--- src/DecisionTree.py
+++ src/DecisionTree.py
@@ -1,14 +1,30 @@
-from sklearn import tree
-import pandas as pd
-
+from sklearn import tree #Scikit-learn Decision tree classifier
+from sklearn.externals import joblib #Replacement for Python's built-in persistence model "pickle", designed for large Numpy arrays
+import pandas as pd #For loading data from csv file
 
 class DecisionTree:
 
-	# Constructor
+	#Constructor
 	def __init__(self, trainingData):
 		self.data = pd.read_csv(trainingData)
 		self.features = self.data.loc[:, 'Curvature':'CarPosition']
 		self.observation = self.data.loc[:, 'Class']
 
+		#Creates a new Decision tree classifier using default parameters
 		self.classifier = tree.DecisionTreeClassifier()
+	
+	#Train the model based on recorded training data
+	def fit(self):
 		self.classifier = self.classifier.fit(self.features, self.observation)
+
+	#Dumps the trained model to a file
+	def dumpModel(self, fileName):
+		joblib.dump(self.classifier, fileName)
+
+	#Loads a trained model from a file
+	def loadModel(self, fileName):
+		self.classifier = joblib.load(fileName)
+
+	#Predicts the output of the trained Decision tree based on input data
+	def predict(self, inputData):
+		return self.classifier.predict(inputData)
