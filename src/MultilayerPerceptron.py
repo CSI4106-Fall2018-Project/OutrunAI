e9c577f07cc212b3a559ce4e3ed6dad52cae0b62
Cooper Lawrence <clawr014@uottawa.ca>
2018-11-19 20:20:26 -0500

    Added constructor and ability to fit, predict, dumpModel, and loadModel

--- src/MultilayerPerceptron.py
+++ src/MultilayerPerceptron.py
@@ -1,7 +1,29 @@
-from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPClassifier #Scikit-learn Multiplayer Perceptron classifier
+from sklearn.externals import joblib #Replacement for Python's built-in persistence model "pickle", designed for large Numpy arrays
+import pandas as pd #For loading data from csv file
 
 class MultilayerPerception:
 
 	#Constructor
-	def __init__(self):
-		pass
+	def __init__(self, trainingData):
+		self.data = pd.read_csv(trainingData)
+		self.features = self.data.loc[:, 'Curvature':'CarPosition']
+		self.observation = self.data.loc[:, 'Class']
+
+		#Creates a new Multilayer Perceptron classifier using default parameters
+		self.classifier = MLPClassifier()
+
+	#Train the model based on recorded training data
+	def fit(self):
+		self.classifier = self.classifier.fit(self.features, self.observation)
+
+	#Dumps the trained model to a file
+	def dumpModel(self, fileName):
+		joblib.dump(self.classifier, fileName)
+
+	#Loads a trained model from a file
+	def loadModel(self, fileName):
+		self.classifier = joblib.load(fileName)
+
+	def predict(self, inputData):
+		return self.classifier.predict(inputData)
